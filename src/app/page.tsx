c90af6736dedc44a1df456c27cfe3bb51ddbeab9
thichlammoctools-collab <thichlammoctools@gmail.com>
2026-03-23 21:58:47 +0700

    tay dổi lớn

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import { redirect } from 'next/navigation';
-
-// Redirect root URL to the products page
-export default function Home() {
-  redirect('/products');
-  return null; // This line will never be reached
-}
